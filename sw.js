3aa75f4618a611631a12e44a2b5130e7995199ad
sebaspreciado99-sys <sebaspreciado99@gmail.com>
2026-05-29 16:01:36 -0500

    Implement service worker for caching assets

--- sw.js
+++ sw.js
@@ -1,1 +1,29 @@
 
+const CACHE_NAME = "trading-journal-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./styles.css",
+  "./app.js",
+  "./manifest.json"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(key => key !== CACHE_NAME ? caches.delete(key) : null))
+    )
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
